697b5d15bdfc7375cf33ddb57528690df2ce7e56
yauhenl <yauhen.lisimau@gmail.com>
2020-01-16 14:53:24 +0300

    lesson 01

--- 1/1.py
+++ 1/1.py
@@ -6,10 +6,6 @@ spaces = rows - 1
 stars = 1
 
 for i in range(rows):
-    print(
-        (SPACE * spaces) +
-        (STRAR * stars) +
-        (SPACE * spaces)
-    )
+    print((SPACE * spaces) + (STRAR * stars) + (SPACE * spaces))
     stars += 2
     spaces -= 1
